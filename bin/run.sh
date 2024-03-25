a29d2827ce5c691baef01f45e93fe2c64758660e
Verina Ong Dinata <verina.od1997@gmail.com>
2024-03-25 22:53:01 +0800

    use env for aws credentials

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -d --name polyglot-container -v $(pwd)/app:/code/app -p 80:80 polyglot
+docker run -d --name polyglot-container -v $(pwd)/app:/code/app -p 80:80 \
+    -e AWS_ACCESS_KEY_ID=[AWS_ACCESS_KEY_ID] -e AWS_SECRET_ACCESS_KEY=[AWS_SECRET_ACCESS_KEY] polyglot
