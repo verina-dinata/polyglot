92edd07f704a6ff8e8d8d62929ac64b6e69deb11
Verina Ong Dinata <verina.od1997@gmail.com>
2024-03-31 14:14:56 +0800

    Setup React material

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, Container, Paper, Typography } from "@mui/material";
 
+
+const serviceList = ["Service 1", "Service 2", "Service 3"]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <Container sx={{ bgcolor: 'tomato', height: '100vh'}}>
+    //   <Typography sx={{ p: 1 }}>Hello World</Typography>
+    // </Container>
+
+    <Container>
+      <Typography variant="h1" sx={{ my:4, textAlign: "center", color: "primary.main" }}>
+        Services
+      </Typography>
+      <Typography variant="h2">
+        Overiew
+      </Typography>
+      <Box
+        sx={{
+          pt: 4,
+          display: 'flex',
+          flexDirection: { xs: "column", md: "row"},
+          justifyContent: "space-between",
+          gap: 4,
+          }}
+      >
+        {serviceList.map((service) => (
+          <Paper elevation={3} sx={{ width: {xs: 1, md: 320}}}>
+            <Box sx={{ m:3 }}>
+              <Typography variant="h3">{service}</Typography>
+              <Typography sx={{ mt:2 }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a dolor aliquet, tincidunt tortor nec, gravida eros. Quisque pulvinar nunc non sapien imperdiet, ac tempus diam feugiat. Sed urna enim, ultrices et elementum vel, malesuada eu lacus. Nullam est orci, lobortis at
+              </Typography>
+              <Button variant="contained" color="secondary" sx={{ mt:2 }}>Learn more</Button>
+            </Box>
+
+          </Paper>
+        ))}
+      </Box>
+
+    </Container>
+  )
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  pallete: {
+    primary: {
+      main: "#013e87",
+    },
+    secondary: {
+      main: "#2e74c9",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: "600",
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: "600",
+    },
+    h3: {
+      fontSize: "1.5rem",
+      fontWeight: "600",
+    },
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
